f7de705790b4f238cf676b76d0719e8bce29baa6
Beagle <al.vazquez@gmail.com>
2025-03-03 22:48:14 -0500

    Configure nvim-tree plugin, with bindings
    nvim-tree is a file explorer for neovim
    See: https://github.com/nvim-tree/nvim-tree.lua
    - <leader>a toggles the navigator
    - <leader>ff jumps to the file of the current buffer in the navigator

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -8,5 +8,20 @@ return {
     {'pmizio/typescript-tools.nvim'},
     {'mhartington/formatter.nvim'},
     {'dense-analysis/ale'},
-    {'nvim-tree/nvim-tree.lua'},
+    {
+        'nvim-tree/nvim-tree.lua', opts = {}, config = true, init = function()
+            vim.g.loaded_netrw = 1       -- disables built in file browser
+            vim.g.loaded_netrwPlugin = 1 -- disables built in file browser
+        end,
+        keys = {
+            {
+                "<leader>a", ":NvimTreeToggle<cr>",
+                desc = "Toggle tree panel"
+            },
+            {
+                "<leader>ff", ":NvimTreeFindFile<cr>",
+                desc = "Open tree to file in current buffer"
+            }
+        }
+    },
 }
