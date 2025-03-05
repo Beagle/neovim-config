14986920923a013e63c3d38a6beaa30f5904316a
Beagle <al.vazquez@gmail.com>
2025-03-05 17:06:30 -0500

    Move nvim-tree and which-key to utils.lua

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -1,41 +1,12 @@
 return {
     {'folke/tokyonight.nvim', lazy = false, priority = 1000},
-    {'williamboman/mason.nvim'}, -- rust
-    {'williamboman/mason-lspconfig.nvim'}, -- rust
-    {'simrat39/rust-tools.nvim', lazy = true}, -- rust
+    {'neovim/nvim-lspconfig'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'simrat39/rust-tools.nvim'}, -- rust
     {'nvim-treesitter/nvim-treesitter'},
     {'mfussenegger/nvim-lint'},
     {'pmizio/typescript-tools.nvim', lazy = true},
     {'mhartington/formatter.nvim'},
     {'dense-analysis/ale'},
-    {
-        'nvim-tree/nvim-tree.lua', opts = {}, config = true, init = function()
-            vim.g.loaded_netrw = 1       -- disables built in file browser
-            vim.g.loaded_netrwPlugin = 1 -- disables built in file browser
-        end,
-        keys = {
-            {
-                "<leader>a", ":NvimTreeToggle<cr>",
-                desc = "Toggle tree panel"
-            },
-            {
-                "<leader>ff", ":NvimTreeFindFile<cr>",
-                desc = "Open tree to file in current buffer"
-            }
-        }
-    },
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        opts = { },
-        keys = {
-            {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
-            },
-        },
-    }
 }
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -38,4 +38,34 @@ return {
             },
         }
     },
+    {
+        'nvim-tree/nvim-tree.lua', opts = {}, config = true, init = function()
+            vim.g.loaded_netrw = 1       -- disables built in file browser
+            vim.g.loaded_netrwPlugin = 1 -- disables built in file browser
+        end,
+        keys = {
+            {
+                "<leader>a", ":NvimTreeToggle<cr>",
+                desc = "Toggle tree panel"
+            },
+            {
+                "<leader>ff", ":NvimTreeFindFile<cr>",
+                desc = "Open tree to file in current buffer"
+            }
+        }
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = { },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    }
 }
