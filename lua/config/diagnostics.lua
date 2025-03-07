f16233e967067e2ad13fd5e44c3bff9d2511f653
Beagle <al.vazquez@gmail.com>
2025-03-06 20:33:25 -0500

    Improve diagnostics config and add toggle
    - <leader>x now toggles diagnostics visbility in current buffer

--- lua/config/diagnostics.lua
+++ lua/config/diagnostics.lua
@@ -2,7 +2,7 @@
 -- LSP Diagnostics Options Setup --
 -----------------------------------
 
--- This configuration sets up icons to be used in the gutter for Diagnostics
+-- Configure how diagnostics is rendered and behaves
 
 -- first, create a function to make this easier
 local sign = function(opts)
@@ -19,26 +19,42 @@ sign({name = 'DiagnosticSignWarn', text = '⚠'})
 sign({name = 'DiagnosticSignHint', text = '🔍'})
 sign({name = 'DiagnosticSignInfo', text = ''})
 
--- Configure the neovim diagnostics behavior
-vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = false,
+-- Define diagnostic configuration
+local diagnosticConfig = {
     float = {
         border = 'rounded',
-        source = 'always',
         header = '',
         prefix = '',
+        source = true,
     },
-})
+    severity_sort = false,
+    signs = true,
+    underline = true,
+    update_in_insert = true,
+    virtual_text = true,
+}
+
+-- Configure the neovim diagnostics behavior
+vim.diagnostic.config(diagnosticConfig)
 
 -- Fixed column for diagnostics to appear
 -- Show autodiagnostic popup on cursor hover_range
 -- Goto previous / next diagnostic warning / error
 -- Show inlay_hints more frequently
 vim.cmd([[
-set signcolumn=yes
+set signcolumn=auto:2
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]])
+
+-- Create toggle to show/hide diagnostics
+local diagnostics_active = true
+local toggle_diagnostics = function()
+  diagnostics_active = not diagnostics_active
+  if diagnostics_active then
+    vim.diagnostic.show(nil, 0, nil, diagnosticConfig)
+  else
+    vim.diagnostic.hide()
+  end
+end
+
+vim.keymap.set('n', '<leader>x', toggle_diagnostics, {desc = "Toggle diagnostics"})
