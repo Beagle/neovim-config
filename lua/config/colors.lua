492b3412abedd052ab38920a5e1ee0af1d7dd97c
Beagle <al.vazquez@gmail.com>
2025-03-06 19:47:31 -0500

    Remove disabled color code

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -20,10 +20,3 @@ require("tokyonight").setup({
 })
 
 vim.cmd.colorscheme("tokyonight") -- specify colorscheme to use
-
---vim.cmd[[hi DiffText guibg=red guifg=black]]
---vim.cmd[[hi Folded ctermfg=Black ctermbg=DarkGrey guifg=black guibg=DarkGrey]]
-
---local fn  = vim.fn
---local signcolumncolor = fn.synIDattr(fn.synIDtrans(fn.hlID('Number')), 'bg')
---vim.cmd[[hi SignColumn guibg=signcolumncolor]]
